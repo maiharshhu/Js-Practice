8a32a9c168ab66dca199a160fc48eefcb78f2cca
Maiharshhu <hj.harsh440@gmail.com>
2025-01-05 04:29:01 +0530

    'conversions'

---
+++ 01.basics/03_conversionop.js
@@ -0,0 +1,38 @@
+// let score = "33abc"
+// console.log(typeof score); // string
+// console.log(typeof (score)); //string
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);// if score is '123cl' means alphanumeric then it will give NaN(NotaNumber) as a O\P
+
+
+let score = null
+
+//true // give 1
+//false // give 0
+// undefined  //it will give NaN
+//null //give 0 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0 
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0=> false
+//"" =>false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
