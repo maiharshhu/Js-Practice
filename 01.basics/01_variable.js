60c25b14fb6149d6ea798e5d7be3b0ecb0806ea0
Maiharshhu <hj.harsh440@gmail.com>
2025-01-05 04:01:22 +0530

    datatypes

---
+++ 01.basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 144453
+let accountEmail = "harsh@google.com"
+var accountPassword = '12435'
+accontCity = "Jaipur"
+//we can define a variable with using const ,let and var but it is not recommended at all
+let accountState;
+
+accountEmail = "har@gmail.com"
+accountPassword = "21212154"
+accontCity = "hyderabad"
+
+console.log(accountId);
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+let s = [accountId, accountEmail, accontCity, accountState]
+
+console.table([accountId, accountEmail, accontCity, accountState])
+
+console.log(s[3])
---
+++ 01.basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version once you use use strict you can't use there is not no strict thing in JS
+
+/*         
+alert(3+3);
+alert("hello")*/ // we are using node.js not browser
+
+console.log("hello"); console.log(3 + 3) // please don not use this as practice it is not recommended at all code readability should be high
+
+let name = 'hitesh'
+let age = 19
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean  =>true/false
+//null => standalone value
+//undefined =>
+//symbol => unique 
+
+//object 
+
+console.log(typeof "harsh");
+console.log(typeof undefined);
+console.log(typeof null);
+
