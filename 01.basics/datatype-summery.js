869b99edfe39802e03eaeceef4bf555c2ef70c82
Maiharshhu <hj.harsh440@gmail.com>
2025-01-06 17:42:18 +0530

    Refactor datatype summary with examples for stack and heap storage

--- 01.basics/datatype-summery.js
+++ 01.basics/datatype-summery.js
@@ -10,18 +10,48 @@
 
 // Array, Objects, Functions
 
-const heros = ["saktimaan", "naagraj", "doga"]
-console.log(typeof heros)
+// const heros = ["saktimaan", "naagraj", "doga"]
+// console.log(typeof heros)
 
-let myObj = {
-    name: "harsh",
-    age: 22,
+// let myObj = {
+//     name: "harsh",
+//     age: 22,
+// }
+
+// console.log(typeof myObj)
+
+// const Myfunction = function () {
+//     console.log("hello world");
+// }
+
+// console.log(typeof Myfunction)
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non-Primitive)Refrence
+
+//stack example
+//Below is a example of how data is stored in stack so after storing if you want value inside another variable so it will give a copy of actual value/data and if you make any changes in this data it will not change the original value/data.
+let myrealname = 'Harshjain'
+
+let myusername = myrealname
+
+myusername = "maiharshhu"
+
+console.log(myrealname)
+console.log(myusername)
+
+// Heap example
+//Below is a example of how data is stored in heap so after storing if you want value inside another variable so it will give will give you a actual value/data or refrence to original value/data and if you make any changes in this then it will changes original value/data.
+
+let userOne = {
+    email: "Harsh@gmail.com",
+    upiid: "23479283@upi",
 }
 
-console.log(typeof myObj)
+let userTwo = userOne
 
-const Myfunction = function () {
-    console.log("hello world");
-}
+userTwo.email = "maiharshhu@gmail.com"
 
-console.log(typeof Myfunction)
+console.log(userOne)
+console.log(userTwo)
