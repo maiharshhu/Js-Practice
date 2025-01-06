0d3d50c3093857a44e4f95f71cc23fcaf3915ae9
Maiharshhu <hj.harsh440@gmail.com>
2025-01-06 16:47:24 +0530

    summery

---
+++ 01.basics/datatype-summery.js
@@ -0,0 +1,27 @@
+//Types of datatype
+
+// primitive
+
+// 7types : string , Number, boolean, null, undefined, symbol, BigInt
+
+
+
+//Refrence (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["saktimaan", "naagraj", "doga"]
+console.log(typeof heros)
+
+let myObj = {
+    name: "harsh",
+    age: 22,
+}
+
+console.log(typeof myObj)
+
+const Myfunction = function () {
+    console.log("hello world");
+}
+
+console.log(typeof Myfunction)
