2e895f9c3e2e30231f234d069e5d0ea794c671ca
Maiharshhu <hj.harsh440@gmail.com>
2025-01-08 16:23:51 +0530

    objects

---
+++ 02.basics/03_objects.js
@@ -0,0 +1,42 @@
+//Singleton
+
+const mySym = Symbol("key1")
+
+//Object literals
+const JsUser = {
+    name: "Harsh",
+    "full name": "Harsh Jain",
+    [mySym]: "mykey1",
+    age: 25,
+    location: "Hyderabad",
+    email: "harrrsh@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// //To access the data from any object we use can do like below example
+// console.log(JsUser.email)
+// console.log(JsUser['email'])
+// //But if your key is string then you need to use as a string in bracket
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym]) //here if you want to use as symbol and print then need to use [] brackets
+
+//// To override any value of object we can achieve by doing .
+JsUser.email = "hj.440@gmail.com"
+
+//freeze - Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
+// Object.freeze(JsUser)
+JsUser.email = "jainharsh@gmail.com"
+// console.log(JsUser)
+
+JsUser.greeting = function () {
+    console.log("Hello Js user");
+}
+
+// // Here using this kw we can access all the elements of object
+JsUser.greetingTwo = function () {
+    console.log(`Hello Js User, ${this.name}`);
+}
+
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
